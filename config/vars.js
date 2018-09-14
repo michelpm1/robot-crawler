c4163ade5524d35c7aa34b7c8389f16998137496
Michel <michelpm1@gmail.com>
2018-09-13 23:21:05 -0300

    fixes

--- config/vars.js
+++ config/vars.js
@@ -2,14 +2,10 @@ const path = require('path');
 
 // import .env variables
 require('dotenv-safe').load({
-    path: path.join(__dirname, '../.env'),
-    sample: path.join(__dirname, '../.env.example')
-});
+    path: path.join(__dirname, '../.env')});
 
 module.exports = {
     env: process.env.DE_ENV,
     port: process.env.PORT,
-    logs: process.env.DE_ENV === 'production' ? 'combined' : 'dev',
-    rateLimitTime: process.env.RATE_LIMIT_TIME,
-    rateLimitRequest: process.env.RATE_LIMIT_REQUEST
+    logs: process.env.DE_ENV === 'production' ? 'combined' : 'dev'
 };
